3d2de14b5720f7bda57b5078bb5d79d0b8200613
MAtterholt <matterholt84@gmail.com>
2019-07-05 21:11:59 -0400

    express routes

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+//console.log("Hello Node.js project.");
+//console.log(process.env.MY_SECRET);
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/test", (req, res) => {
+  res.send("Test World!");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}`)
+);
