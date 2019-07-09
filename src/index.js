803620610992923bc9deec2f6c656c8a5d83b574
MAtterholt <matterholt84@gmail.com>
2019-07-08 23:38:08 -0400

    added express middle ware, POST

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+// Express Middleware
+import uuidv4 from "uuid/v4";
+import bodyParser from "body-parser";
 
 //console.log("Hello Node.js project.");
 //console.log(process.env.MY_SECRET);
@@ -33,8 +36,16 @@ let messages = {
 };
 //declared function
 const app = express();
-// ******************
-app.use;
+// *********EXPRESS MIDDLEWARE *********
+// parses the text as JSON and exposes the resulting object on req.body
+app.use(bodyParser.json());
+// text as URL encode data, send from data from regular forms set POST
+app.use(bodyParser.urlencoded({ extended: true }));
+// *********CUSTOM MIDDLEWARE *********
+app.use((req, res, next) => {
+  // do something this is how to start the custom middleware
+  next();
+});
 
 // CRUD HTTP methods and REST Operations
 // two routes for reading the whole list of users and a single user by ID
@@ -58,6 +69,19 @@ app.get("/messages/:messagesId", (req, res) => {
 app.post("/users", (req, res) => {
   return res.send("Received a POST HTTP method");
 });
+
+app.post("/messages", (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text
+  };
+  messages[id] = message;
+  return res.send(message);
+});
+
+// ***************************
+
 app.put("/users/:userId", (req, res) => {
   return res.send(`PUT HTTP method on user/${req.params.userId} resource \n`);
 });
