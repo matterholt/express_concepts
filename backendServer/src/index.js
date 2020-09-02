3ac8c62772cee4a57c2b7c50cb0fe8f78b030a56
MAtterholt <matterholt84@gmail.com>
2020-09-02 14:48:10 -0400

    file rename

---
+++ backendServer/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import routes from "./routes/index";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World! this will build");
+});
+
+app.use(express.static("public"));
+
+app.use("/request", routes.request);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+});
---
+++ backendServer/src/routes/request.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { Sequelize } from "sequelize";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello World! from request");
+});
+
+export default router;
